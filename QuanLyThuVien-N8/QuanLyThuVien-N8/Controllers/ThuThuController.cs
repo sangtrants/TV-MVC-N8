cf22407857859b5a73c42280ec827d648d33a393
Tan Nguyen <trongtan.yoyo@gmail.com>
2014-11-20 00:06:57 +0700

    Quản Lý Độc Giả - Tấn
    Thêm, xóa, sửa và quản lý ảnh đại diện độc giả.

--- QuanLyThuVien-N8/QuanLyThuVien-N8/Controllers/ThuThuController.cs
+++ QuanLyThuVien-N8/QuanLyThuVien-N8/Controllers/ThuThuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 using QuanLyThuVien_N8.Models;
 
 namespace QuanLyThuVien_N8.Controllers
