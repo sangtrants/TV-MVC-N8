c641907a462b35d50e77e5fb950ddaa59136d8c1
Nguyễn Đức Quý <ducquy.1993@gmail.com>
2014-11-21 20:35:17 +0700

    Quý
    Update BarCode

---
+++ QuanLyThuVien-N8/QuanLyThuVien-N8/Controllers/BarcodeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace QuanLyThuVien_N8.Controllers
+{
+    public class BarcodeController : Controller
+    {
+        //
+        // GET: /Barcode/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetStatus()
+        {
+            return View();
+        }
+
+        public ActionResult CreateBarCodeView()
+        {
+            string data = Request["txtBarcode"];
+            string path = Path.Combine( HttpRuntime.AppDomainAppPath ,"BarCode\\Created\\" , data + ".jpg");
+            QuanLyThuVien_N8.Models.BarCode b = new Models.BarCode(data,path);
+            b.CreateBarCode(data, path);
+            return View(b);
+        }
+        public ActionResult GetBarCodeView()
+        {
+            return View();
+        }
+        public ActionResult GetBarCodeView_Camera()
+        {
+            return View();
+        }
+        public ActionResult GetBarCodeView_File()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult SaveImage()
+        {
+            if (Request.InputStream.Length > 0)
+            {
+                using (StreamReader reader = new StreamReader(Request.InputStream))
+                {
+                    string hexString = Server.UrlEncode(reader.ReadToEnd());
+                    string imageName = DateTime.Now.ToString("dd-MM-yy hh-mm-ss");
+                    string imagePath = string.Format("/BarCode/Captured/{0}.png", imageName);
+                    string imageFullPath = Server.MapPath(imagePath);
+                    MakeSureDirectoryExists(Path.GetDirectoryName(imageFullPath));
+                    System.IO.File.WriteAllBytes(Server.MapPath(imagePath), ConvertHexToBytes(hexString));
+                    //string[] result = BarcodeLib.BarcodeReader.BarcodeReader.read(imageFullPath, BarcodeLib.BarcodeReader.BarcodeReader.CODABAR);
+                    //string kq = result[0];
+                }
+            }
+
+            return Json(new { success = true });
+        }
+
+        private static void MakeSureDirectoryExists(string directory)
+        {
+            if (Directory.Exists(directory)) { return; }
+
+            Directory.CreateDirectory(directory);
+        }
+
+        private static byte[] ConvertHexToBytes(string hex)
+        {
+            byte[] bytes = new byte[hex.Length / 2];
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
+
+    }
+}
---
+++ QuanLyThuVien-N8/QuanLyThuVien-N8/Models/BarCode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyThuVien_N8.Models
+{
+    public class BarCode
+    {
+        public string Data;
+        public string FullPath;
+        public BarCode(string data, string path)
+        {
+            this.Data = data;
+            this.FullPath = path;
+        }
+        public string CreateBarCode(string data, string path)
+        {
+            string kq = "Thanh Cong";
+            BarcodeLib.Barcode.Linear codabar = new BarcodeLib.Barcode.Linear();
+            codabar.Type = BarcodeLib.Barcode.BarcodeType.CODABAR;
+            codabar.Data = data;
+            string imageFullPath = path;
+            this.FullPath = imageFullPath;
+            codabar.UOM = BarcodeLib.Barcode.UnitOfMeasure.PIXEL;
+            codabar.BarColor = System.Drawing.Color.Blue;
+            codabar.BarWidth = 2;
+            codabar.LeftMargin = 12;
+            codabar.RightMargin = 12;
+            codabar.drawBarcode(imageFullPath);
+            string kt = "";
+            try
+            {
+                string[] result = BarcodeLib.BarcodeReader.BarcodeReader.read(imageFullPath, BarcodeLib.BarcodeReader.BarcodeReader.CODABAR);
+                kt = result[0];
+                kq = imageFullPath;
+                return kq;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                kq = "Wrong input";
+                return kq;
+            }
+        }
+        public string ReadBarCode(string path)
+        {
+            string kq = "";
+            try
+            {
+                string[] result = BarcodeLib.BarcodeReader.BarcodeReader.read(path, BarcodeLib.BarcodeReader.BarcodeReader.CODABAR);
+                kq = result[0];
+                return kq.Substring(1, kq.Length - 2);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                return kq;
+            }
+        }
+    }
+}
